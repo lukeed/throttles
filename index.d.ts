c0d84aa8875fcce822c0d9d3e4d691367d836d78
Luke Edwards <luke.edwards05@gmail.com>
2019-08-21 16:29:23 -0700

    chore: rename types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 type isDone = () => void;
 type toAdd = (fn: Function) => void;
-declare function throttles(limit?: number = 1): [toAdd, isDone];
+declare function throttles(limit?: number): [toAdd, isDone];
 export default throttles;
